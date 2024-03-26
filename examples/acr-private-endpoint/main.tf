aec13f397c89f071870f907348365e6458d373bc
dsahoo <debasish.sahoo@nttdata.com>
2024-03-25 23:28:48 -0400

    'Migrate repository from old GitHub organization.'

--- examples/acr-private-endpoint/main.tf
+++ examples/acr-private-endpoint/main.tf
@@ -11,7 +11,7 @@
 // limitations under the License.
 
 module "resource_names" {
-  source = "git::https://github.com/nexient-llc/tf-module-resource_name.git?ref=1.0.0"
+  source = "git::https://github.com/launchbynttdata/tf-launch-module_library-resource_name.git?ref=1.0.0"
 
   for_each = var.resource_names_map
 
@@ -25,7 +25,7 @@ module "resource_names" {
 }
 
 module "resource_group" {
-  source = "git::https://github.com/nexient-llc/tf-azurerm-module_primitive-resource_group.git?ref=0.2.0"
+  source = "git::https://github.com/launchbynttdata/tf-azurerm-module_primitive-resource_group.git?ref=1.0.0"
 
   name     = module.resource_names["rg"].standard
   location = var.region
@@ -35,7 +35,7 @@ module "resource_group" {
 }
 
 module "vnet" {
-  source = "git::https://github.com/nexient-llc/tf-azurerm-module_primitive-virtual_network.git?ref=0.1.0"
+  source = "git::https://github.com/launchbynttdata/tf-azurerm-module_primitive-virtual_network.git?ref=1.0.0"
 
   vnet_location                                         = var.region
   resource_group_name                                   = module.resource_group.name
@@ -62,7 +62,7 @@ module "vnet" {
 }
 
 module "acr" {
-  source = "git::https://github.com/nexient-llc/tf-azurerm-module_primitive-container_registry.git?ref=0.3.0"
+  source = "git::https://github.com/launchbynttdata/tf-azurerm-module_primitive-container_registry.git?ref=1.0.0"
 
   container_registry_name       = module.resource_names["acr"].lower_case_without_any_separators
   location                      = var.region
@@ -78,7 +78,7 @@ module "acr" {
 }
 
 module "private_dns_zone" {
-  source = "git::https://github.com/nexient-llc/tf-azurerm-module_primitive-private_dns_zone.git?ref=0.1.0"
+  source = "git::https://github.com/launchbynttdata/tf-azurerm-module_primitive-private_dns_zone.git?ref=1.0.0"
 
   zone_name           = "privatelink.azurecr.io"
   resource_group_name = module.resource_group.name
@@ -89,7 +89,7 @@ module "private_dns_zone" {
 }
 
 module "vnet_link" {
-  source = "git::https://github.com/nexient-llc/tf-azurerm-module_primitive-private_dns_vnet_link.git?ref=0.1.0"
+  source = "git::https://github.com/launchbynttdata/tf-azurerm-module_primitive-private_dns_vnet_link.git?ref=1.0.0"
 
   link_name             = "acr-pe-link"
   private_dns_zone_name = module.private_dns_zone.zone_name
--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module github.com/nexient-llc/tf-azurerm-module_primitive-private_endpoint
+module github.com/launchbynttdata/tf-azurerm-module_primitive-private_endpoint
 
 go 1.20
 
 require (
 	github.com/gruntwork-io/terratest v0.43.12
-	github.com/nexient-llc/lcaf-component-terratest-common v0.0.0-20240117163707-a1dfafae58b4
+	github.com/launchbynttdata/lcaf-component-terratest v0.0.0-20240117163707-a1dfafae58b4
 	github.com/stretchr/testify v1.8.1
 )
 
